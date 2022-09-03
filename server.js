51690b379937a78fc0f4a834ba62c7b7aa530ab0
Natxafav <Natidevg@gmail.com>
2022-09-03 01:39:14 +0100

    Finalizado Backend

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config(); //estamos pidiendo el .env
-const cors = require("cors");
+const cors = require("cors"); // evitar problemas server 3000 con 5000
 
 const rutaDocente = require("./routes/rutas-docente");
 const rutaCurso = require("./routes/rutas-curso");
